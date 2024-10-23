3361eb5bcfccf9bcfb1f325177e940e4e0518a92
kazukiotsuka <otsuka.kazuki@googlemail.com>
2024-10-23 06:06:22 +0000

    interview list ui

--- web-server/views/src/js/view_controllers/interview_home_view_controller.js
+++ web-server/views/src/js/view_controllers/interview_home_view_controller.js
@@ -2,96 +2,138 @@ document.addEventListener('DOMContentLoaded', () => {
     const lang = window.lang;
     const locale = window.locale;
     console.log(`set up materials view.\nlang:${lang}`);
-  
+
     // initialize root view
     let materialsView = new MaterialsView({
-      id: 'MainContent',
-      locale: locale,
-      lang: lang
+        id: 'MainContent',
+        locale: locale,
+        lang: lang
     });
-  });
-  class InterviewHomeViewController {
+});
+class InterviewHomeViewController {
     constructor({
-      id,
-      locale,
-      lang = 'en'
+        id,
+        locale,
+        lang = 'en'
     }) {
-      this.id = id;
-      this.locale = locale;
-      this.lang = lang;
-      this.interviews = [];
-      this.loadInterviews();
-      console.log(`Initialize ${this.id}: with material ${material}`);
+        this.id = id;
+        this.locale = locale;
+        this.lang = lang;
+        this.interviews = [];
+        this.setupView();
+        this.loadInterviews();
+        console.log(`Initialize ${this.id}: with material ${material}`);
+    }
+    setupView() {
+        this.$view = document.getElementById("InterviewHomeView");
+
+        const $listContainer = document.createElement('div');
+        $listContainer.id = 'InterviewListContainer';
+        $listContainer.classList.add('InterviewListContainer');
+        this.$view.appendChild($listContainer);
     }
     fetchUser(onSuccess) {
-      fetch(`/v1/${this.lang}/user`).then(response => {
-        if (!response.ok) {
-          return response.json().then(errData => {
-            console.error(`Error fetching user: ${errData.code} ${errData.message}`);
-            if (response.status === 401 || response.status === 404) {
-              console.log('Redirecting to login page...');
-              window.location.href = `/v1/${lang}/signup`;
+        fetch(`/v1/${this.lang}/user`).then(response => {
+            if (!response.ok) {
+                return response.json().then(errData => {
+                    console.error(`Error fetching user: ${errData.code} ${errData.message}`);
+                    if (response.status === 401 || response.status === 404) {
+                        console.log('Redirecting to login page...');
+                        window.location.href = `/v1/${lang}/signup`;
+                    }
+                    return Promise.reject(errData);
+                });
             }
-            return Promise.reject(errData);
-          });
-        }
-        return response.json();
-      }).then(data => {
-        const {
-          user,
-          code,
-          message
-        } = data;
-        console.log('User data fetched:', user);
-        onSuccess(user);
-      }).catch(error => {
-        console.error('Unexpected error occurred when init:', error);
-      });
+            return response.json();
+        }).then(data => {
+            const {
+                user,
+                code,
+                message
+            } = data;
+            console.log('User data fetched:', user);
+            onSuccess(user);
+        }).catch(error => {
+            console.error('Unexpected error occurred when init:', error);
+        });
     }
     loadInterviews() {
-      this.loading(true);
-      Http.get(`/v1/${this.lang}/interviews/list`, res => {
-        this.loading(false);
-        const {
-          interviews,
-          count
-        } = res;
-        this.interviews = interviews;
-        debuglog(interviews);
-        if (this.interviews.length == 0) {
-          this.fetchUser(user => {
-            this.openInterviewCreateModalView(user, this.locale, this.lang);
-          });
-        }
-        // DEBUG 
-        else {
-          this.fetchUser(user => {
-            this.openInterviewCreateModalView(user, this.locale, this.lang);
-          });
-        }
-        // DEBUG
-      }, error => {
-        this.loading(false);
-        // TODO: show error message
-      });
+        this.loading(true);
+        Http.get(`/v1/${this.lang}/interviews/list`, res => {
+            this.loading(false);
+            const {
+                interviews,
+                count
+            } = res;
+            this.interviews = interviews;
+            debuglog(interviews);
+            if (this.interviews.length == 0) {
+                this.fetchUser(user => {
+                    this.openInterviewCreateModalView(user, null, this.locale, this.lang);
+                });
+            }
+            else {
+                this.setupInterviewListView(this.lang, this.locale);
+            }
+            //// DEBUG 
+            //else {
+            //    this.fetchUser(user => {
+            //        this.openInterviewCreateModalView(user, interviews[0].id, this.locale, this.lang);
+            //    });
+            //}
+            //// DEBUG
+        }, error => {
+            this.loading(false);
+            // TODO: show error message
+        });
     }
-    openInterviewCreateModalView(user, locale, lang) {
-      this.interviewCreateModalView = new InterviewCreateModalView({
-        id: 'InterviewCreateModalView',
-        user: user,
-        locale: locale,
-        lang: lang,
-        title: "Create New Interview",
-        //locale.get(MaterialsLocaleKeys.SETTINGS_MODAL_VIEW_TITLE, lang),
-        text: "",
-        cancelButtonText: "Cancel",
-        //locale.get(MaterialsLocaleKeys.SETTINGS_MODAL_VIEW_CANCEL, lang),
-        doneButtonText: "Done",
-        //locale.get(MaterialsLocaleKeys.SETTINGS_MODAL_VIEW_DONE, lang),
-        shouldCloseOnTapBG: true
-      });
-      this.interviewCreateModalView.mount('#MainContent');
-      this.interviewCreateModalView.show();
+
+    setupInterviewListView(lang, locale) {
+        this.interviewList = new InterviewList({
+            id: 'interviewList',
+            lang: lang,
+            createNewButtonTitle: locale.get("create_new_interview_button_title", lang),
+            headerTitle: locale.get('interview_list_header_title', lang),
+            listCountTextSingular: locale.get('interview_list_count_singular', lang),
+            listCountTextPlural: locale.get('interview_list_count_plural', lang),
+        });
+        this.interviewList.$view.addEventListener("clickedInterviewCell", (event)=> {
+            const { index, interviewId, cell } = event.detail;
+            this.openInterviewCreateModalView(this.user, interviewId, this.locale, this.lang);
+        })
+        this.interviewList.$view.addEventListener("clickedNewInterviewButton", (event)=> {
+            this.openInterviewCreateModalView(this.user, null, this.locale, this.lang);
+        })
+        //this.interviewList.loadinterviews();
+        this.interviewList.updateContentsFromInterviews(this.interviews);
+        this.interviewList.appendToElementById('InterviewListContainer');
+        this.interviewList.loading(false);
+    }
+
+    openInterviewCreateModalView(user, interviewId, locale, lang) {
+        let title = "";
+        if (interviewId) {
+            title = locale.get("interview_create_page_title_edit", lang);
+        } else {
+            title = locale.get("interview_create_page_title_new", lang);
+        }
+        this.interviewCreateModalView = new InterviewCreateModalView({
+            id: 'InterviewCreateModalView',
+            user: user,
+            locale: locale,
+            lang: lang,
+            title: title,
+            //locale.get(MaterialsLocaleKeys.SETTINGS_MODAL_VIEW_TITLE, lang),
+            text: "",
+            interviewId: interviewId, //'670dcf37aa9bfc2db50d1574'
+            cancelButtonText: "Cancel",
+            //locale.get(MaterialsLocaleKeys.SETTINGS_MODAL_VIEW_CANCEL, lang),
+            doneButtonText: "Done",
+            //locale.get(MaterialsLocaleKeys.SETTINGS_MODAL_VIEW_DONE, lang),
+            shouldCloseOnTapBG: true
+        });
+        this.interviewCreateModalView.mount('#MainContent');
+        this.interviewCreateModalView.show();
     }
     loading(isLoading) {}
-  }
+}
